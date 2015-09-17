a94a367bad2eebb3b6ccf2971ddde3e5ff752323
Christian Wörner <christianworner@gmail.com>
2015-09-17 23:40:37 +0100

    tests: simple IP pattern test

--- tests/test_logfile.py
+++ tests/test_logfile.py
@@ -11,7 +11,7 @@ Tests for `logfile` module.
 import pytest
 from collections import OrderedDict, Counter
 
-from apalog import logfile
+from apalog import logfile, patterns
 
 
 @pytest.fixture
@@ -70,3 +70,9 @@ def test_classify_and_count_with_split_days(sample_log):
         ('2015-09-01', Counter({'200': 35, '301': 2, '403': 2, '404': 3})),
         ('2015-09-02', Counter({'200': 24, '404': 24, '405': 1}))])
     assert sample_log.split_days().classify_and_count('HTTP/1.[10]" (...)') == expected
+
+
+def test_classify_and_count_IP_addresses(sample_log):
+    expected = [('151.27.127.120', 36), ('206.99.94.219', 16), ('177.3.165.141', 5),
+                ('199.58.86.209', 4), ('104.148.44.116', 2)]
+    assert sample_log.classify_and_count(patterns.IP_V4_ADDRESS).most_common(5) == expected
