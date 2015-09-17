a994f9b103158af0e3f00f098e94b2f5e7211994
Christian Wörner <christianworner@gmail.com>
2015-09-17 23:24:08 +0100

    patterns: add a generic IP v4 pattern

--- apalog/patterns.py
+++ apalog/patterns.py
@@ -4,3 +4,4 @@ HTTP_STATUS_TEMPLATE = 'HTTP/1.[10]" {}'
 HTTP_STATUS_GROUP = 'HTTP/1.[10]" (...)'
 
 DATE_PATTERN = "[0-9]{2}/[a-zA-Z]{3}/[0-9]{4}"
+IP_V4_ADDRESS = '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}'
